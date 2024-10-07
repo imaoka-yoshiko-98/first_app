c05129675bd639fd35637694ca9d1491e32544f0
imaoka-yoshiko-98 <jiazijingang@gmail.com>
2024-10-07 17:45:58 +0900

    first-commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def index  
+    @posts = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"  # すべてのレコードを@postsに代入
+  end
+ 
+    def new
+  end
+  
+  def create
+  Post.create(content: params[:content])
+  redirect_to "/posts"
+  
+ 
+end
+
+
+
+
